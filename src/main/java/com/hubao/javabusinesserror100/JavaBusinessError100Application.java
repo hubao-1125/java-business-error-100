e2b594e4bb634a0cd2c9474887b3ab36a536ab9d
douhu <douhu@oristartech.com>
2021-04-26 15:50:02 +0800

    init pro

---
+++ src/main/java/com/hubao/javabusinesserror100/JavaBusinessError100Application.java
@@ -0,0 +1,13 @@
+package com.hubao.javabusinesserror100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBusinessError100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBusinessError100Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hubao/javabusinesserror100/JavaBusinessError100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubao.javabusinesserror100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBusinessError100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
